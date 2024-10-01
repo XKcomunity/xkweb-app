d94cc90d878ce9f6e3ee05c63a24a1045ef3778e
Michael Linares <michaellinaresdbildungs@gmail.com>
2024-10-01 21:31:07 +0200

    Fixed snippetTech card bug.

--- src/app/snippets/[snippets_tech]/SnippetCard.tsx
+++ src/app/snippets/[snippets_tech]/SnippetCard.tsx
@@ -12,25 +12,27 @@ export default function SnippetCard({ snippetsType }: Props) {
 
 	return (
 		<section className={styles.snippet_card_container}>
-			<article key={snippet.id} className={styles.snippet_card}>
-				<Link href={`/snippets/${snippet.tech}/${snippet.id}`}>
-					<div className={styles.snippet_card_heading}>
-						<Image
-							src={snippet.img.url}
-							className={styles.snippet_card_img}
-							alt="Snippet card image"
-							width={100}
-							height={100}
-						/>
-						<div className={styles.snippet_card_heading_text}>
-							<h3>{snippet.title}</h3>
-							<p className={styles.snippet_card_paragraph}>
-								{snippet.description.slice(0, 300)}...
-							</p>
+			{snippetsType.map((snippet) => (
+				<article key={snippet.id} className={styles.snippet_card}>
+					<Link href={`/snippets/${snippet.tech}/${snippet.id}`}>
+						<div className={styles.snippet_card_heading}>
+							<Image
+								src={snippet.img.url}
+								className={styles.snippet_card_img}
+								alt="Snippet card image"
+								width={100}
+								height={100}
+							/>
+							<div className={styles.snippet_card_heading_text}>
+								<h3>{snippet.title}</h3>
+								<p className={styles.snippet_card_paragraph}>
+									{snippet.description.slice(0, 300)}...
+								</p>
+							</div>
 						</div>
-					</div>
-				</Link>
-			</article>
+					</Link>
+				</article>
+			))}
 		</section>
 	);
 }
